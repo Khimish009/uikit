f2cd8336ba585add88dda409ff21c9061ce96dc0
Khimish009 <salman.khimishev01@gmail.com>
2023-10-20 21:47:02 +0300

    feat: add global types

--- tags_input/src/TagsInput/TagsInput.tsx
+++ tags_input/src/TagsInput/TagsInput.tsx
@@ -1,86 +1,95 @@
 import { ChangeEvent, KeyboardEvent, useState } from "react";
-import './TagsInput.scss'
+// import './TagsInput.scss'
 
 interface TagsInputProps {
-    tags: string[];
-    onChange: (tags: string[]) => void;
+  tags: string[];
+  onChange: (tags: string[]) => void;
 }
 
 export const TagsInput = ({ tags, onChange }: TagsInputProps) => {
-    const [value, setValue] = useState("");
+  const [value, setValue] = useState("");
 
-    const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
-        const isEnter = e.code === 'Enter'
-        const value = e.currentTarget.value
+  const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+    const isEnter = e.code === 'Enter'
+    const value = e.currentTarget.value
 
-        if (value.length === 0 || !isEnter) return
+    if (value.length === 0 || !isEnter) return
 
-        setValue('')
-        onChange([...tags, value])
-    };
+    setValue('')
+    onChange([...tags, value])
+  };
 
-    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-        const value = e.target.value;
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
 
-        if (!value.includes(",")) {
-            setValue(value);
-            return;
-        }
+    if (!value.includes(",")) {
+      setValue(value);
+      return;
+    }
 
-        const values = value.split(",");
-        const lastValue = values.pop();
+    const values = value.split(",");
+    const lastValue = values.pop();
 
-        setValue(lastValue || "");
+    setValue(lastValue || "");
 
-        if (values.length === 0) return;
+    if (values.length === 0) return;
 
-        onChange([...tags, ...values]);
-    };
+    onChange([...tags, ...values]);
+  };
 
-    const handleDelete = (tagToDelete: string) => {
-        onChange(tags.filter((tag) => tag !== tagToDelete));
-    };
+  const handleDelete = (tagToDelete: string) => {
+    onChange(tags.filter((tag) => tag !== tagToDelete));
+  };
 
-    return (
-        <div className="tags-input">
-            {tags.map((tag) => (
-                <div
-                    data-testid="tag"
-                    key={tag}
-                    style={{
-                        padding: "3px 8px",
-                        backgroundColor: "lightgrey",
-                        borderRadius: 3,
-                        display: "flex",
-                        alignItems: "center"
-                    }}
-                >
-                    {tag}
-                    <div
-                        style={{
-                            height: "100%",
-                            marginLeft: 4,
-                            cursor: "pointer"
-                        }}
-                        onClick={() => handleDelete(tag)}
-                        data-testid="tag-delete"
-                    >
-                        x
-                    </div>
-                </div>
-            ))}
-            <input
-                value={value}
-                onChange={handleChange}
-                onKeyDown={handleKeyDown}
-                style={{
-                    border: "none",
-                    outline: "none",
-                    flexBasis: "120px"
-                }}
-                type="text"
-                data-testid="tags-input"
-            />
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexWrap: "wrap",
+        gap: 6,
+        padding: "6px 12px",
+        border: "1px solid lightgray",
+        borderRadius: 8
+      }}
+    >
+      {tags.map((tag) => (
+        <div
+          data-testid="tag"
+          key={tag}
+          style={{
+            padding: "3px 8px",
+            backgroundColor: "lightgrey",
+            borderRadius: 3,
+            display: "flex",
+            alignItems: "center"
+          }}
+        >
+          {tag}
+          <div
+            style={{
+              height: "100%",
+              marginLeft: 4,
+              cursor: "pointer"
+            }}
+            onClick={() => handleDelete(tag)}
+            data-testid="tag-delete"
+          >
+            x
+          </div>
         </div>
-    );
+      ))}
+      <input
+        value={value}
+        onChange={handleChange}
+        onKeyDown={handleKeyDown}
+        style={{
+          border: "none",
+          outline: "none",
+          flexBasis: "120px"
+        }}
+        type="text"
+        data-testid="tags-input"
+      />
+    </div>
+  );
 };
